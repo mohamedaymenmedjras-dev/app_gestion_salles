914933597f6cf35b15681e8bd6284c35584f276c
Medjras aymen mohamed amine <mohamedaymenmedjras@gmail.com>
2026-04-18 22:11:14 -0400

    creation la methode update salle

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -22,6 +22,12 @@ class DataSalle:
         sql = "insert into salle values (%s,%s,%s,%s)"
         values = (salle.code,salle.description,salle.categorie,salle.capacite)
         cursor.execute(sql,values)
+    def update_salle(self,salle):
+        con = self.get_connection()
+        cursor = con.cursor()
+        sql = ("update salle set description = %s, categorie = %s,capacite = %s where code = %s")
+        values = (salle.description,salle.categorie,salle.capacite,salle.code)
+        cursor.execute(sql,values)
 
         con.commit()
         cursor.close()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data.dao_salle import DataSalle
 from models.salle import Salle
 data=DataSalle()
-s1 = Salle("z32", "Salle laboratoire chimique.", "chimique", 33)
-data.insert_salle(s1)
+s1 = Salle("z32", "Salle laboratoire chimique.", "chimique", 12)
+data.update_salle(s1)
